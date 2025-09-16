17c502a0cefc916712313e0574c9a817ebd6899b
Jhon Maximiliano <jhondmaxv@gmail.com>
2025-09-15 23:44:32 -0500

    Update persistence.tf

--- persistence.tf
+++ persistence.tf
@@ -1,13 +1,18 @@
 resource "docker_container" "postgres" {
   name  = "postgres-${terraform.workspace}"
   image = "postgres:13-trixie"
-  environment = [
+
+  env = [
     "POSTGRES_PASSWORD=${var.postgres_password}"
   ]
 
   ports {
     internal = 5432
     external = var.postgres_external_port[terraform.workspace]
+  }
+
+  networks_advanced {
+    name = docker_network.persistence_network.name
   }
 }
 
@@ -16,7 +21,11 @@ resource "docker_container" "redis" {
   image = "redis:latest"
 
   ports {
-    internal = 6379 
+    internal = 6379
     external = var.redis_external_port[terraform.workspace]
+  }
+
+  networks_advanced {
+    name = docker_network.persistence_network.name
   }
 }
