f3816857aac182e0bb4be2c6e98b053f52eb38e8
Jhon Maximiliano <jhondmaxv@gmail.com>
2025-09-15 23:42:59 -0500

    Update nginx.tf

--- nginx.tf
+++ nginx.tf
@@ -1,29 +1,41 @@
 resource "docker_container" "app1" {
-  name  = "nginx-app1${terraform.workspace}"
+  name  = "nginx-app1-${terraform.workspace}"
   image = "nginx:stable-perl"
 
   ports {
     internal = 80
-    external = var.nginx_external_port[terraform.workspace]
+    external = var.nginx_app1_external_port[terraform.workspace]
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
   }
 }
 
 resource "docker_container" "app2" {
-  name  = "nginx-app2${terraform.workspace}"
+  name  = "nginx-app2-${terraform.workspace}"
   image = "nginx:stable-perl"
 
   ports {
     internal = 80
-    external = var.nginx_external_port[terraform.workspace]
+    external = var.nginx_app2_external_port[terraform.workspace]
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
   }
 }
 
 resource "docker_container" "app3" {
-  name  = "nginx-app3${terraform.workspace}"
+  name  = "nginx-app3-${terraform.workspace}"
   image = "nginx:stable-perl"
 
   ports {
     internal = 80
-    external = var.nginx_external_port[terraform.workspace]
+    external = var.nginx_app3_external_port[terraform.workspace]
+  }
+
+  networks_advanced {
+    name = docker_network.app_network.name
   }
 }
